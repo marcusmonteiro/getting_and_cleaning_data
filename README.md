c27523bbdeb0d0f3f9eba85c96aaf91c680fb892
Marcus Vinicius Monteiro de Souza <mvsouza007@gmail.com>
2017-05-23 22:35:43 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Getting and Cleaning Data
+
+My files from the Getting and Cleaning Data Coursera course.
---
+++ run_analysis.R
@@ -0,0 +1,83 @@
+LoadRequiredPackages <- function() {
+  # Load the required packages.
+  if (!require('pacman')) {
+    install.packages('pacman')
+  }
+  pacman::p_load(dplyr, readr)
+}
+
+dataset.dir <- 'UCI HAR Dataset'
+  
+DownloadDataset <- function() {
+  # Download dataset if the dataset directory does not exists.
+  if (!dir.exists(dataset.dir)) {
+    dataset.url <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
+    temp <- tempfile()
+    download.file(dataset.url, temp)
+    unzip(temp)
+  }
+}
+
+MakeTidyDataset <- function() {
+  ### Tyding up test and train sets
+  
+  path.test <- file.path(dataset.dir, 'test')
+  path.train <- file.path(dataset.dir, 'train')
+  
+  data.test.set <- read_table(file.path(path.test, 'X_test.txt'), 
+                              col_names = FALSE)
+  data.train.set <- read_table(file.path(path.train, 'X_train.txt'), 
+                               col_names = FALSE)
+  data.merged.set <- bind_rows(data.test.set, data.train.set)
+  
+  # read_table results in a single column with '1 tBodyAcc-mean()-X' like values :( ...
+  data.features <- read.table(file.path(dataset.dir, 'features.txt'), 
+                              stringsAsFactors = FALSE)
+  data.features <- rename(data.features, FeatureCode = V1 , FeatureName = V2)
+  data.features <- filter(data.features, grepl("mean\\(\\)|std\\(\\)", FeatureName))
+  # Matching the X in data.merged.set
+  data.features <- mutate(data.features, FeatureCode = paste0("X", FeatureCode))
+  
+  data.merged.set <- select(data.merged.set, one_of(data.features$FeatureCode))
+  setnames(data.merged.set, names(data.merged.set), data.features$FeatureName)
+  
+  ### By now data.merget.set contains only mean and std data, and more descriptive
+  ### labels.
+  ### Start working on subject and activity label data
+  
+  data.test.subject <- read_table(file.path(path.test, 'subject_test.txt'), 
+                                  col_names = FALSE)
+  data.train.subject <- read_table(file.path(path.train, 'subject_train.txt'), 
+                                   col_names = FALSE)
+  data.merged.subject <- bind_rows(data.test.subject, data.train.subject)
+  data.merged.subject <- rename(data.merged.subject, Subject = X1)
+  
+  data.test.activity <- read_table(file.path(path.test, 'y_test.txt'), 
+                                  col_names = FALSE)
+  data.train.activity <- read_table(file.path(path.train, 'y_train.txt'), 
+                                   col_names = FALSE)
+  data.merged.activity <- bind_rows(data.test.activity, 
+                                          data.train.activity)
+  data.merged.activity <- rename(data.merged.activity, 
+                                       ActivityLabel = X1)
+  
+  data.activity.label <- read_table(file.path(dataset.dir, 'activity_labels.txt'), 
+                                    col_names = FALSE)
+  data.activity.label <- rename(data.activity.label, ActivityLabel = X1, 
+                                Activity = X2)
+  
+  data.merged.activity <- full_join(data.merged.activity, data.activity.label)
+  data.merged.activity <- select(data.merged.activity, -ActivityLabel)
+  
+  data.merged.subject <- bind_cols(data.merged.subject, data.merged.activity)
+  data.merged.set <- bind_cols(data.merged.subject, data.merged.set)
+  
+  data.merged.set
+}
+
+run <- function() {
+  print(LoadRequiredPackages())
+  DownloadDataset()
+  file.remove('foo.csv')
+  write.csv(MakeTidyDataset(), file = 'foo.csv')
+}
